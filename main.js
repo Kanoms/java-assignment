5bf6f695aea6e53e83bdfe63bc3797fb660293c7
Kelechi <kanomneze@gmail.com>
2023-05-12 06:24:35 +0100

    final commit

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+
+function calcTwo(event) {
+  event .preventDefault();
+  const num1 = document.getElementById("num1").value
+  const num2 = document.getElementById("num2").value
+
+  let sum = parseInt(num1) + parseInt(num2);
+  document.getElementById("result").textContent = "Sum: " + sum;
+  console.log(sum);
+};
+
 function calc(event) {
   event .preventDefault();
-  const valueOne = document.getElementById("valueOne").value
-  const valueTwo = document.getElementById("valueTwo").value
+  const num1 = document.getElementById("num1").value
+  const num2 = document.getElementById("num2").value
 
-  let result = valueOne ** valueTwo;
+  let result = parseInt(num1) ** parseInt(num2);
+  document.getElementById("result").textContent = "Exponent: " + result;
   console.log(result);
-}
+};
+
